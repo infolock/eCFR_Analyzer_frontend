5695d5f5f8a2ecb70bb1f042b7f2d0b5f3cef202
Jonathon Hibbard <jonathon.hibbard@engineeredinnovationgroup.com>
2025-02-13 20:35:34 -0500

    update readme.  initial import of frontend service

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WordCountDisplay from './WordCountDisplay';
+
+function App() {
+  return (
+    <div className="App">
+      <WordCountDisplay />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/WordCountDisplay.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+function WordCountDisplay() {
+    const [wordCounts, setWordCounts] = useState({});
+
+    useEffect(() => {
+        fetch("http://localhost:5001/api/word_counts")
+            .then(res => res.json())
+            .then(data => setWordCounts(data))
+            .catch(err => console.error("Error fetching data:", err));
+    }, []);
+
+    return (
+        <div>
+            <h2>Word Count by Agency</h2>
+            <ul>
+                {Object.entries(wordCounts).map(([agency, count]) => (
+                    <li key={agency}>{agency}: {count} words</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default WordCountDisplay;
