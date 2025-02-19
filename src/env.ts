5c13a1952e45c29476a0655c9f1fc5f82e0063a5
Jonathon Hibbard <jonathon.hibbard@engineeredinnovationgroup.com>
2025-02-18 21:55:58 -0500

    update to request https

--- src/env.ts
+++ src/env.ts
@@ -2,5 +2,5 @@ export const getDomain = (): string => {
     if (localStorage.getItem('ecrf_dev') === 'dev') {
         return 'http://localhost:5001';
     }
-    return 'http://ecfranalyzerbackend-production.up.railway.app';
+    return 'https://ecfranalyzerbackend-production.up.railway.app';
 };
